ab5489fabf7bf1b3afe573955e283553366579d7
omergat <gateniomer@gmail.com>
2022-04-15 18:12:06 +0300

    ...

--- src/app.js
+++ src/app.js
@@ -66,12 +66,21 @@ function init(){
   updateNoteList();
 
   // Check that service workers are supported
-  if ('serviceWorker' in navigator) {
-  // Use the window load event to keep the page load performant
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/Note-App/service-worker.js');
-  });
-}
+//   if ('serviceWorker' in navigator) {
+//   // Use the window load event to keep the page load performant
+//   window.addEventListener('load', () => {
+//     navigator.serviceWorker.register('/Note-App/service-worker.js');
+//   });
+// }
+ if ('serviceWorker' in navigator) {
+   window.addEventListener('load', () => {
+     navigator.serviceWorker.register('/Note-App/service-worker.js').then(registration => {
+       console.log('SW registered: ', registration);
+     }).catch(registrationError => {
+       console.log('SW registration failed: ', registrationError);
+     });
+   });
+ }
 }
 
 init();
