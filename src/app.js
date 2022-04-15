359ce32090d83ec8a021475b56d4bf7bf3f1e973
omergat <gateniomer@gmail.com>
2022-04-15 17:56:34 +0300

    ...

--- src/app.js
+++ src/app.js
@@ -69,7 +69,7 @@ function init(){
   if ('serviceWorker' in navigator) {
   // Use the window load event to keep the page load performant
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/Note-App/service-worker.js');
+    navigator.serviceWorker.register('/service-worker.js');
   });
 }
 }
