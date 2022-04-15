c830ffd70a3eb76f8fc7c0aa9251daa6028ef374
omergat <gateniomer@gmail.com>
2022-04-15 17:22:27 +0300

    updated files

--- src/app.js
+++ src/app.js
@@ -64,6 +64,14 @@ function init(){
 
   //Update Note List
   updateNoteList();
+
+  // Check that service workers are supported
+  if ('serviceWorker' in navigator) {
+  // Use the window load event to keep the page load performant
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js');
+  });
+}
 }
 
 init();
--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ module.exports = {
   mode: 'development',
   entry: {
     bundle: path.resolve(__dirname, 'src/app.js'),
+    'service-worker': "./src/service-worker.js"
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
