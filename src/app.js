ff99269e90279252a9106779baa8fbed6d73c1c8
omergat <gateniomer@gmail.com>
2022-04-15 18:19:56 +0300

    ...

--- src/app.js
+++ src/app.js
@@ -74,7 +74,7 @@ function init(){
 // }
  if ('serviceWorker' in navigator) {
    window.addEventListener('load', () => {
-     navigator.serviceWorker.register('/Note-App/service-worker.js').then(registration => {
+     navigator.serviceWorker.register('/service-worker.js').then(registration => {
        console.log('SW registered: ', registration);
      }).catch(registrationError => {
        console.log('SW registration failed: ', registrationError);
